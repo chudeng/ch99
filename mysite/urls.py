bf5984d962cd7352b73891d4fbd2fd2c0823c64f
From Human <chudeng@hanmail.net>
2021-05-10 11:48:01 +0900

    ch9 revision 1. templates folder

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,12 +22,12 @@ from mysite.views import UserCreateView, UserCreateDoneTV
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomeView.as_view(), name='home'),
     path('bookmark/', include('bookmark.urls')),
     path('blog/', include('blog.urls')),
     path('photo/', include('photo.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/register/', UserCreateView.as_view(), name='register'),
     path('accounts/register/done/', UserCreateDoneTV.as_view(), name='register_done'),
+    path('', HomeView.as_view(), name='home'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
