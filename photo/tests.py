30d8f327bc8718654824c46113bebd4d789a66e7
From Human <chudeng@hanmail.net>
2021-05-10 17:37:16 +0900

    ch9 revision a little test to fix photo app

--- photo/tests.py
+++ photo/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class Person:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.set_age(age)
+
+    def get_age(self):
+        return self._age
+
+    def set_age(self, age):
+        if age < 0:
+            raise ValueError("Invalid age")
+        self._age = age
+
+    age = property(get_age, set_age)
+
+person = Person('김', '아무개', 20)
+print(person.get_age())
+
+
+person.set_age(person.get_age()+1)
+print(person.get_age())
